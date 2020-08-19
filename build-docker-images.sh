6e22591361da2b6954c09aa5fe795cd1313ffd44
ceeac <ceeac@users.noreply.github.com>
2020-08-19 10:20:58 +0200

    Add github actions workflow

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,5 +2,5 @@
 
 for img in $(ls dockerfiles); do
 	sudo docker build -t ceeac/simutrans-build-env:$img dockerfiles/$img/
-	echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USERNAME --password-stdin && sudo docker push ceeac/simutrans-build-env:$img
+	echo $DOCKER_PASSWORD | sudo docker login -u ceeac --password-stdin && sudo docker push ceeac/simutrans-build-env:$img
 done
